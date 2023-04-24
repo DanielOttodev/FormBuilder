ceee7350ee089d09549dfecafcd17864432a6a7c
danielottodev <otto_281@hotmail.com>
2023-04-24 23:46:05 +1000

    ui movers

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import FormEngine from './FormEngine'
-
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 
 
 function App() {
 
   return (
 
-   
+
     <div className='grid grid-cols-5 gap-3'>
       <div className='border rounded shadow p-10 col-span-5'>
         <div className="flex justify-center">
@@ -21,11 +21,25 @@ function App() {
           </a>
         </div>
         <h1 className='font-bold'>Form Engine</h1>
-        <FormEngine ></FormEngine>
+        <DragDropContext>
+          <Droppable droppableId='dropzone'>
+            {(provided) =>(
+              <div 
+              {...provided.droppableProps}  
+              ref={provided.innerRef}  >
+              
+               <FormEngine ></FormEngine>
+              </div>
+                     
+
+            )}
+          </Droppable>
+
+        </DragDropContext>
+
       </div>
 
     </div>
-
     
   )
 }
--- src/FormEngine/index.jsx
+++ src/FormEngine/index.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
-import { BsPlusCircle, BsXCircle, BsClipboard } from "react-icons/bs";
-
+import { BsPlusCircle, BsXCircle, BsClipboard, BsPencilFill,BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill,BsFillArrowUpSquareFill,BsFillArrowDownSquareFill } from "react-icons/bs";
+import {FaUndo} from "react-icons/fa"
+import { Draggable } from "react-beautiful-dnd";
 
 export default function FormEngine(){
 
+    const [previousField,setPreviousField] = useState([]);
     const [fieldSet,setFieldSet] = useState([
-        {Fields:[{type:'text',placeholder:'Name',id:'drag21'}]},
-        {Fields:[{type:'number',placeholder:'Phone',id:'drag22'},{type:'text',placeholder:'Email',id:'drag23'},{type:'number',placeholder:'ABN',id:'drag24'}]}
+        {Fields:[{type:'text',placeholder:'Name',id:'drag21'}],id:'row1'},
+        {Fields:[{type:'number',placeholder:'Phone',id:'drag22'},{type:'text',placeholder:'Email',id:'drag23'},{type:'number',placeholder:'ABN',id:'drag24'}],id:'row2'},
+        {Fields:[{type:'text',placeholder:'Test2',id:'drag25'},{type:'number',placeholder:'Test3',id:'drag26'}],id:'row3'}
     ]);
 
     const [formData,setFormData] = useState({
@@ -18,6 +21,7 @@ function removeField(e){
     let dataTarget = e.target.getAttribute('data-target')
     let arr = dataTarget.split('-');
     setFieldSet((prev) => {
+        setPreviousField(prev);
         const newData = prev.map(items => ({
             ...items,
             Fields: items.Fields.filter(item => item.id !== arr[1])
@@ -35,6 +39,9 @@ function submit(){
 function addRow(){
     alert('Lets make a new row!')
 }
+function undo(){
+setFieldSet(previousField);
+}
 
 let rowFields;
 let allRows = []
@@ -42,21 +49,21 @@ let allRows = []
 for(let x = 0; x < fieldSet.length; x ++){
 // Loop through the main fields array to determine the amount of rows; Loop through the row fields to determine the amount of columns & fields in the row. 
 // Calculate how many columns in the row
+
 let gridCount;
-if(fieldSet[x].Fields.length >= 4)  gridCount = 4;
-if(fieldSet[x].Fields.length === 3)  gridCount = 3;
-if(fieldSet[x].Fields.length === 2)  gridCount = 2;
-if(fieldSet[x].Fields.length === 1)  gridCount = 1;
+gridCount = fieldSet[x].Fields.length
 
 rowFields = fieldSet[x].Fields.map((columnField,y) => {
     return <div key={y}>
-                <div className="container relative rounded border-dotted cursor-move group m-2">
+                <div className="container relative rounded border-dotted cursor-pointer group ">
                     <div className="overlay flex hidden group-hover:block border-2 border-gray-500 border-dashed rounded">
-                    <p>Drag to reposition</p>
-
-                    <button data-target={x + '-' +columnField.id} onClick={removeField} className="p-1 rounded-md text-center align-middle text-xs cursor-pointer">Delete Field <BsXCircle size={14} className=" inline text-red-600 mx-2"></BsXCircle> </button>
-                    <button className="p-1 rounded-md ml-1 text-center align-middle text-xs cursor-pointer">Copy Field <BsClipboard size={14} className=" inline text-green-600 mx-2"></BsClipboard> </button>
-                    <h4></h4>
+                    <button className="p-1 rounded-md mx-1 text-center align-middle text-xs cursor-pointer hover:bg-slate-100">Add<BsPlusCircle size={14} className=" inline text-green-600 mx-2"></BsPlusCircle> </button>
+                    <button className="p-1 rounded-md mx-1 text-center align-middle text-xs cursor-pointer hover:bg-slate-100">Edit <BsPencilFill size={14} className=" inline text-blue-600 mx-2"></BsPencilFill> </button>
+                    <button data-target={x + '-' +columnField.id} onClick={removeField} className="hover:bg-red-600 p-1 rounded-md text-center align-middle text-xs cursor-pointer mx-1 bg-red-500 text-white">Delete<BsXCircle size={14} className="text-white inline text-red-600 mx-2"></BsXCircle> </button>
+                    <div className="flex justify-between">
+                       <BsFillArrowLeftCircleFill data-target={x + '-' +columnField.id} onClick={() => {alert('Moving!')}} size={20} className="ml-3"></BsFillArrowLeftCircleFill>
+                       <BsFillArrowRightCircleFill data-target={x + '-' +columnField.id} size={20} className="mr-3"></BsFillArrowRightCircleFill>
+                    </div>
                     </div>
                     <input  className="text-2xl p-2 m-1  border-2 rounded w-full outline-gray-300" type={columnField.type} placeholder={columnField.placeholder}></input>
                 </div>
@@ -64,26 +71,39 @@ rowFields = fieldSet[x].Fields.map((columnField,y) => {
    
 })
 // Render the row with it's respective columns
-console.log(gridCount)
-let row = 
-            <div  className={'grid gap-x-3 mx-auto grid-cols-'+gridCount} key={x}>
-                {rowFields}
-            </div>
-          
+console.log('RowFields:',rowFields);
+
+let row = <div className="group/row ease-in hover:border-x-2 rounded border-sky-300">
+        <BsFillArrowUpSquareFill size={24} className="hidden group-hover/row:block text-sky-300 hover:cursor-pointer"></BsFillArrowUpSquareFill>
+            <div key={x}
+         className={'  grid gap-x-3 p-2 mx-auto grid-cols-'+gridCount}>
+        {rowFields}
+        </div>
+        <BsFillArrowDownSquareFill size={24} onClick={() =>{ alert('clicked')}} className="hidden group-hover/row:block text-sky-300 hover:cursor-pointer"></BsFillArrowDownSquareFill>
+</div>
+
+       
+console.log('Row:',row);        
 allRows.push(row);
 }
+
+console.log('AR:',allRows)
 return(
-    <div className="m-3 container mx-auto">
+    <div>
+    <div className="m-3 container mx-auto  ">
     <h3 className="text-3xl">{formData.form.name}</h3>
     {allRows}
 
+
+    <div>
+    <button onClick={submit} className="bg-sky-500 px-4 py-2 text-white rounded hover:outline-gray-300 mt-5 outline-none">Submit</button>
+    </div>
     <div className="text-center">
     <button onClick={addRow} className="p-2 rounded mt-5 text-gray-400 outline-dashed outline-1 hover:outline-gray-500 focused:outline-none">Add Row<BsPlusCircle size={24} className="inline ml-2 text-gray-400" ></BsPlusCircle></button>
     </div>
-    <div>
-    <button onClick={submit} className="bg-sky-500 px-4 py-2 text-white rounded hover:outline-gray-300 mt-5 outline-none">Submit</button>
-    
     </div>
+    {previousField.length >= 1 && <button onClick={undo} className="ml-2 p-1 rounded hover:text-sky-500 text-sm text-gray-500 "><FaUndo size={16} className="inline"></FaUndo></button> }
+
     </div>
 
 )}
