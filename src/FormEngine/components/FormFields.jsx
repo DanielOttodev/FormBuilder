baf305302f45cf17111655055aebec5752a5d054
danielottodev <otto_281@hotmail.com>
2023-05-02 22:22:04 +1000

    checkboxes

--- src/FormEngine/components/FormFields.jsx
+++ src/FormEngine/components/FormFields.jsx
@@ -15,7 +15,7 @@ if(html === 'textarea'){
     <textarea required={required} className="text-1xl p-1 m-1  border-2 rounded w-full outline-gray-300" type={type} placeholder={placeholder}></textarea>
     </div>
 }
-if(html === 'select'){
+if(html === 'checkbox'){
     return <div className="flex gap-5 py-2">
            {values.map((x,index) => { return <div key={x}> 
            <input type="checkbox" className="mr-2 w-4 h-4 inline" value={x}   />
